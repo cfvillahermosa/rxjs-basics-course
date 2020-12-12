efbf9d55bd75d21bdfa3db88604b39f92b7f4ded
Carlos Villa <carlos.fernandezv@vass.es>
2020-12-12 23:31:24 +0100

    Create your first observable

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-import { of } from 'rxjs';
-
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
+import { Observable } from 'rxjs';
+/**
+ * Observers can register up to three callbacks to be invoked by the observable
  */
-of('Hello', 'RxJS').subscribe(console.log);
+const observer = {
+    next: value => console.log('next', value), // for successfully emitted values
+    error: error => console.log('error', error), // to forward any errors that can occur withing your observable execution
+    complete: () => console.log('complete!') // to be notified that no values will be deliver
+};
+/*
+* creates a new observable
+* observable constructor takes a function that receives a subscriber
+* subscriber is an enhanced version of the observer you supply when you subscribe in order to properly manage behavior like error handling and completion
+*/
+const observable = new Observable(subscriber => {
+    subscriber.next('Hello');
+    subscriber.next('World');
+    subscriber.complete();
+    // once is completed no more values will be delivered
+    subscriber.next('Hello');
+    subscriber.next('World');
+});
+
+observable.subscribe(observer);
