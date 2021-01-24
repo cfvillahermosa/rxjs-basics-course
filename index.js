9652fa9513d20782d4f301b1f4adf8fe8569a9e3
Carlos Fernández Villahermosa <cfvillahermosa@gmail.com>
2021-01-25 00:41:08 +0100

    Deliver values asynchronously with observables

--- index.js
+++ index.js
@@ -13,14 +13,23 @@ const observer = {
 * subscriber is an enhanced version of the observer you supply when you subscribe in order to properly manage behavior like error handling and completion
 */
 const observable = new Observable(subscriber => {
-    subscriber.next('Hello');
-    subscriber.next('World');
-    subscriber.complete();
+    let count = 0;
 
+    // we delivers values asynchronously using set interval
+    const id = setInterval(() => {
+        subscriber.next(count);
+        if(count === 4)
+        subscriber.complete();
+        count++;
+    }, 1000);
+
+    // then we return a function to clear the interval when observable completes, IMPORTANT: this section is only returned when observable completes
+    return (() => {
+        console.log('called');
+        clearInterval(id);
+    })
 });
 
-observable.subscribe(value =>
-    console.log('next', value)
-    // null,
-    // () => console.log('complete!')
-);
+console.log('before');
+observable.subscribe(observer);
+console.log('after');
